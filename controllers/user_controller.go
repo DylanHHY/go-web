ff85278c7b3be1f1b9d47eb1cc97a49cbe36005e
Dylan <dyan2009200@gmail.com>
2024-04-19 08:34:05 +0800

    Add login and Register

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,1 +1,42 @@
 package controllers
+
+import (
+	DB "go-side-project/initializers"
+	model "go-side-project/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Login(c *gin.Context){
+	message := "Hello World"
+	text := "Welcome to world of Golang again :)))"
+	c.JSON(http.StatusOK, gin.H{"message": message, "text": text})
+}
+
+func Register(c *gin.Context){
+	var user model.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad request data"})
+		return
+	}
+	user.Password = user.Password + "safety"
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to encrypt password"})
+        return
+    }
+
+	user.Password = string(hashedPassword)
+	user.Role = "USER"
+
+	if err := DB.DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register user"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User registered successfully"})
+
+
+}
--- router/routers.go
+++ router/routers.go
@@ -1,8 +1,9 @@
 package router
 
 import (
-	controllers "go-side-project/controllers"
+	"go-side-project/controllers"
 	"net/http"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/static"
@@ -18,17 +19,41 @@ func InitRouter() *gin.Engine {
 	config.AllowHeaders = append(config.AllowHeaders, "Authorization")
 
 	router.Use(cors.New(config))
-	router.Use(static.Serve("/", static.LocalFile("./fronted", false)))
+	router.Use(static.Serve("/", static.LocalFile("./frontend", false)))
 
-	router.GET("/api/v1/welcome", func(c *gin.Context) {
-		message := "Hello World"
-		text := "Welcome to world of Golang again :)))"
-		c.JSON(http.StatusOK, gin.H{"message": message, "text": text})
-	})
+	api := router.Group("api/v1")
+	{
+		api.GET("/welcome", controllers.HomeIndex)
+		api.POST("/login", controllers.Login)
+		api.POST("/register", controllers.Register)
+	}
 
-	router.GET("/", controllers.HomeIndex)
+	router.GET("/", renderHTMLPage)
+	router.GET("/login", renderHTMLPage)
+	router.GET("/register", renderHTMLPage)
 	// router.POST("/api/register", controllers.Register)
 	// router.POST("api/login", controllers.Login)
 
 	return router
+}
+
+func renderHTMLPage(c *gin.Context){
+    var filePath string
+    switch c.Request.URL.Path {
+    case "/login":
+        filePath = "login.html"
+    case "/register":
+        filePath = "register.html"
+    default:
+        filePath = "index.html"
+    }
+
+    // 檢查文件是否存在
+    if _, err := os.Stat("./fronted/" + filePath); os.IsNotExist(err) {
+        c.String(http.StatusNotFound, "File not found")
+        return
+    }
+
+    // 返回 HTML 文件
+    c.File("./fronted/" + filePath)
 }
