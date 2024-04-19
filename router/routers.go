60657be8a9a9e8c49ff3b2404cfe725c95cfebef
dylan.huang <dylan.huang@silicon-core.com>
2024-04-19 19:02:15 +0800

    Modify router.go

--- router/routers.go
+++ router/routers.go
@@ -3,7 +3,6 @@ package router
 import (
 	"go-side-project/controllers"
 	"net/http"
-	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/static"
@@ -28,32 +27,15 @@ func InitRouter() *gin.Engine {
 		api.POST("/register", controllers.Register)
 	}
 
-	router.GET("/", renderHTMLPage)
-	router.GET("/login", renderHTMLPage)
-	router.GET("/register", renderHTMLPage)
-	// router.POST("/api/register", controllers.Register)
-	// router.POST("api/login", controllers.Login)
+	router.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/index.html")
+	})
+	router.GET("/login", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/login.html")
+	})
+	router.GET("/register", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/register.html")
+	})
 
 	return router
-}
-
-func renderHTMLPage(c *gin.Context){
-    var filePath string
-    switch c.Request.URL.Path {
-    case "/login":
-        filePath = "login.html"
-    case "/register":
-        filePath = "register.html"
-    default:
-        filePath = "index.html"
-    }
-
-    // 檢查文件是否存在
-    if _, err := os.Stat("./fronted/" + filePath); os.IsNotExist(err) {
-        c.String(http.StatusNotFound, "File not found")
-        return
-    }
-
-    // 返回 HTML 文件
-    c.File("./fronted/" + filePath)
 }
